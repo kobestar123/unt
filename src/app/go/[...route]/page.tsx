b8f194459edcd6a4115c760f1f8ce85b42124027
kobestar123 <jakobycavitt0@gmail.com>
2024-09-24 19:40:52 -0400

    Update page.tsx

--- src/app/go/[...route]/page.tsx
+++ src/app/go/[...route]/page.tsx
@@ -82,7 +82,7 @@ export default function Route({ params }: { params: { route: string[] } }) {
           </Button>
           <div className="flex items-center gap-2">
             {tabIcon ? <img src={tabIcon} className="h-8 w-8" /> : <Lucide.Radius className="h-8 w-8 rotate-180" />}
-            <h1 className="text-xl font-bold">{tabName ? tabName : 'Radius'}</h1>
+            <h1 className="text-xl font-bold">{tabName ? tabName : 'Untitled'}</h1>
           </div>
         </div>
         <div className="flex items-center gap-2 z-50">
