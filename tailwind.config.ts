ff296cf9148164a9d94d2e98f8560ade14da49d4
kobestar123 <jakobycavitt0@gmail.com>
2024-09-24 19:25:15 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,9 +56,9 @@ const config = {
         }
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+        lg: 'var(--untitled)',
+        md: 'calc(var(--untitled) - 2px)',
+        sm: 'calc(var(--untitled) - 4px)'
       },
       keyframes: {
         'accordion-down': {
